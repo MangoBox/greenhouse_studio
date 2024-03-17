2571db996dead4d1435b55a4e21e238248b096a6
Nymeria <Hugh.Hannan@student.uts.edu.au>
2024-03-18 10:46:18 +1100

    Adding files from master over to main

---
+++ greenhouse_client/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import * as React from 'react';
+import Button from '@mui/joy/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          GREENHOUSE CLIENT DEV
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button variant="outlined">Hello world</Button>
+
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
