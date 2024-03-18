d3f8b8675dcae5a25aaa8e6779413680967f0bda
Liam Davies <mangobox228@hotmail.com.au>
2024-03-18 11:06:39 +1100

    ADDED AWESOME FILESgit status!

--- greenhouse_client/src/App.js
+++ greenhouse_client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          GREENHOUSE CLIENT DEV
+          GREENHOUSE CLIENT DEV - LIAM IS THE BEST!!!!!
         </p>
         <a
           className="App-link"
