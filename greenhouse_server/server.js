61d84ef356a0e41a9df221af4db3353cf8053032
Liam Davies <mangobox228@hotmail.com.au>
2024-03-24 11:55:43 +1100

    added server routes

--- greenhouse_server/server.js
+++ greenhouse_server/server.js
@@ -7,11 +7,20 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
-const dbo = require("./db/conn");
+//const dbo = require("./db/conn");
+
+
+// Sample reply data
+
+
 app.listen(port, () => {
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
+ /* dbo.connectToServer(function (err) {
     if (err) console.error(err);
-   });
+   }); */
   console.log(`Server is running on port: ${port}`);
 });
+
+app.get('/get', (req, res) => {
+  res.send(req.query)
+})
