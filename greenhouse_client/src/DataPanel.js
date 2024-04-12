943ce04a3a75d5bb1db4237fd2ea01cf36e04dde
Liam Davies <mangobox228@hotmail.com.au>
2024-04-12 20:58:01 +1000

    Added additional code comments for clarity.

--- greenhouse_client/src/DataPanel.js
+++ greenhouse_client/src/DataPanel.js
@@ -4,6 +4,9 @@ import DataDisplay from "./DataDisplay";
 import Box from '@mui/joy/Box';
 import CircularProgress from '@mui/joy/CircularProgress';
 
+// This display metadata provides the information needed to display the data.
+// It provides context for the data, including what buttons, suffixes and prefixes
+// are to be provided.
 const displayMetadata = {
     'temperature' :
     {
@@ -21,19 +24,23 @@ const displayMetadata = {
 };
 
 function CreateDisplayComponent(key, points) {
-    //If we haven't specified to display this key, return nothing 
+    //If we haven't specified to display this key, return nothing.
     if(!displayMetadata[key]) {
         return;
     }
 
+    // Automatically formats the data to be displayed, based on the metadata
+    // and providing a fallback value should no metadata be provided.
     var suffix = displayMetadata[key].suffix ?? '';
     var prefix = displayMetadata[key].prefix ?? '';
     var minValue = displayMetadata[key].minValue ?? 0;
     var maxValue = displayMetadata[key].maxValue ?? 100;
     var buttons = displayMetadata[key].buttons ?? null;
+    // Automatically capatalises the first letter of the key, should no title be provided.
     var title = displayMetadata[key].title ?? key.charAt(0).toUpperCase() + key.slice(1);
     var value = points[key] ?? 0;
 
+    // Returns the data display component with all the metadata provided.
     return (
         <Box>
             {DataDisplay(title, value, prefix, suffix, minValue, maxValue, buttons)}
@@ -60,8 +67,11 @@ function DataPanel() {
     var last_updated_display = <Box>Loading...</Box>;;
     if(last_updated != "Loading...") {
         var now = Date.now();
+
+        //Create delta time to display how long ago the data was last updated.
         var delta = now - Date.parse(last_updated);
-        
+
+        //Automatically format days, minutes and hours
         var minutes = Math.floor(delta / (1000 * 60)); 
         if (minutes < 60) {
             last_updated_display = <Box>Last updated: {minutes}min ago</Box>;
@@ -76,23 +86,14 @@ function DataPanel() {
             minutes = minutes % 60;
             last_updated_display = <Box>Last updated: {days}d {hours}h {minutes}min ago</Box>;
         }
-        
     }
-    
 
-    console.log(points);
-
+    //Finally, map the constituent data points when received from the React hook.
     var displays = !points ? <CircularProgress color="success"/> : Object.keys(points).map((key) => 
         <Box>
             {CreateDisplayComponent(key, points)}
         </Box>
     );
-    
-    // points.map((point) => 
-    //         <Box>
-    //             {DataDisplay(point.title, point.value, point.prefix, point.suffix, point.minValue, point.maxValue, point.buttons)}
-    //         </Box>
-    // );
 
     return (
     <>
